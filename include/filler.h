2fa18b48c4bb6c409cb2bfef48ded340f164b6ef
rbarbazz <raphael.barbazza@gmail.com>
2018-06-19 17:33:14 +0200

    singletone ok

--- include/filler.h
+++ include/filler.h
@@ -6,7 +6,7 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 13:25:01 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/06/19 13:26:46 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/06/19 17:16:30 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 # include "libft.h"
 # include "get_next_line.h"
 # include "ft_printf.h"
+
+typedef	struct	s_fill
+{
+	int		p;
+	char	*plateau;
+	char	*piece;
+}				t_fill;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,41 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 13:22:39 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/06/19 13:36:19 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/06/19 17:31:21 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int	main(void)
+int			get_p_number(void)
 {
-	ft_printf("Hello world !\n");
+	char	*line;
+
+	get_stdin(STDIN_FILENO, &line);
+	ft_dprintf(STDERR_FILENO, "%s", line);
+	return (1);
+}
+
+t_fill		*get_fill(void)
+{
+	static t_fill fill;
+
+	return (&fill);
+}
+
+int			main(void)
+{
+	t_fill	*fill;
+
+	fill = get_fill();
+	fill->p = get_p_number();
+	char	*line;
+
+	get_stdin(STDIN_FILENO, &line);
+	ft_dprintf(STDERR_FILENO, "%s", line);
+	while (1)
+	{
+
+	}
+	return (0);
 }
