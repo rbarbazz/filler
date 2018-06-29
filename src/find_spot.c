29f303a753dcf60acedfa04ec2c1228c26ffc405
rbarbazz <raphael.barbazza@gmail.com>
2018-06-29 12:09:16 +0200

    full normed

--- src/find_spot.c
+++ src/find_spot.c
@@ -6,13 +6,13 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/20 21:15:17 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/06/28 13:54:34 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/06/29 12:08:43 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-static int		test_position(t_coord next)
+static int		test_position(t_coord next, char **map, char **piece)
 {
 	int		xx;
 	int		yy;
@@ -27,9 +27,12 @@ static int		test_position(t_coord next)
 		xx = 0;
 		while (xx < fill->size_piece.x)
 		{
-			if (fill->piece[yy][xx] == '*' && (next.y + yy > fill->size_map.y - 1 || next.x + xx > fill->size_map.x - 1 || fill->plateau[next.y + yy][next.x + xx] == fill->advers))
+			if (piece[yy][xx] == '*' && (next.y + yy > fill->size_map.y - 1 ||\
+			next.x + xx > fill->size_map.x - 1 || map[next.y + yy][next.x +\
+			xx] == fill->advers))
 				return (0);
-			else if (fill->piece[yy][xx] == '*' && (fill->plateau[next.y + yy][next.x + xx] == fill->player || fill->plateau[next.y + yy][next.x + xx] == fill->player + 32))
+			else if (piece[yy][xx] == '*' && (map[next.y + yy][next.x + xx] ==\
+			fill->player || map[next.y + yy][next.x + xx] == fill->player + 32))
 				fill->match++;
 			xx++;
 		}
@@ -50,7 +53,7 @@ int				place_piece(void)
 		fill->next.x = 0;
 		while (fill->next.x < fill->size_map.x)
 		{
-			if (test_position(fill->next) == 1)
+			if (test_position(fill->next, fill->plateau, fill->piece) == 1)
 				return (0);
 			fill->next.x++;
 		}
